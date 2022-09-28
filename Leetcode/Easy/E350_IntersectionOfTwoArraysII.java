2a8a32d1b1b53c0f17a689588452b24b87a21e04
senwa105 <senwa105@gmail.com>
2022-09-28 09:40:05 -0400

    added better scaling solution for E350

--- Leetcode/Easy/E350_IntersectionOfTwoArraysII.java
+++ Leetcode/Easy/E350_IntersectionOfTwoArraysII.java
@@ -1,4 +1,6 @@
 import java.util.Stack;
+// import java.util.HashMap;
+// import java.util.ArrayList;
 
 public class E350_IntersectionOfTwoArraysII {
     public static int[] intersect(int[] nums1, int[] nums2) {
@@ -23,4 +25,29 @@ public class E350_IntersectionOfTwoArraysII {
         
         return arr;
     }
+
+    /* 
+    pzhang15's solution
+    My solution ends up being faster, but this solution should scale better
+    once the overhead of the HashMap becomes irrelevant
+    */ 
+    // public static int[] intersect(int[] nums1, int[] nums2) {
+    //     HashMap<Integer, Integer> map = new HashMap<>();
+    //     for(int i : nums1){
+    //         int freq = map.getOrDefault(i, 0);
+    //         map.put(i, freq + 1);
+    //     }
+    //     ArrayList<Integer> list = new ArrayList<>();
+    //     for(int i : nums2){
+    //         if(map.get(i) != null && map.get(i) > 0){
+    //             list.add(i);
+    //             map.put(i, map.get(i) - 1);
+    //         }
+    //     }
+    //     int[] ret = new int[list.size()]; 
+    //     for(int i = 0; i < list.size();i++){
+    //         ret[i] = list.get(i);
+    //     }
+    //     return ret;
+    // }
 }
