8efe9106581a1f467943f49135cc0e27b7cf85e3
senwa105 <senwa105@gmail.com>
2022-10-17 12:54:35 -0400

    added much better solution from the community

--- 1743B.cpp
+++ 1743B.cpp
@@ -12,30 +12,45 @@ typedef pair<ll, ll> pl;
 #define G(x) ll x; cin >> x;                                    // read ll from cin and store its value
 #define F(i, start, end) for (ll i = start; i < (end); i++)     // shortened for loop
 
+// int main() {
+//     ios_base::sync_with_stdio(0);
+//     cin.tie(0);
+
+//     // SOLUTION HERE
+//     G(t);
+//     F(i, 0, t) {
+//         G(n);
+//         ll count = n;
+//         ll inc = 1;
+//         bool add = true;
+
+//         while (count--) {
+//             ll num;
+//             if (add) num = inc;
+//             else num = n - inc++;
+//             if (num == (n+1)/2) {
+//                 cout << n << " ";
+//                 cout << num;
+//                 break;
+//             }
+//             else cout << num << " ";
+//             add = !add;
+//         }
+//         cout << "\n";
+//     }
+// }
+
+// much simpler solution 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
     // SOLUTION HERE
     G(t);
-    F(i, 0, t) {
+    F(tt, 0, t) {
         G(n);
-        ll count = n;
-        ll inc = 1;
-        bool add = true;
-
-        while (count--) {
-            ll num;
-            if (add) num = inc;
-            else num = n - inc++;
-            if (num == (n+1)/2) {
-                cout << n << " ";
-                cout << num;
-                break;
-            }
-            else cout << num << " ";
-            add = !add;
-        }
-        cout << "\n";
+        cout << 1 << " ";
+        F(i, 3, n+1) cout << i << " ";
+        cout << 2 << "\n";
     }
 }
