ab0bab30b28ad0717ed9f07affc4062cd7a9f733
senwa105 <senwa105@gmail.com>
2022-10-20 21:46:39 -0400

    solved 0032B

---
+++ Codeforces/0032B.cpp
@@ -0,0 +1,32 @@
+#pragma GCC target("avx2")
+#pragma GCC optimize("03")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef long double ld;
+typedef pair<ll, ll> pl;
+
+// macros
+#define G(x) ll x; cin >> x;                                    // read ll from cin and store its value
+#define F(i, start, end) for (ll i = start; i < (end); i++)     // shortened for loop
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    // SOLUTION HERE
+    string s; cin >> s;
+
+    F(i, 0, s.length()) {
+        if (s[i] == '.') {
+            cout << 0;
+        }
+        else {      // char is a -
+            i++;
+            if (s[i] == '.') cout << 1;
+            else cout << 2;
+        }
+    }    
+}
