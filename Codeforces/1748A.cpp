5ae7933a31192c3cc60d41d94e7bb214ff8ecb53
senwa105 <senwa105@gmail.com>
2022-11-20 01:59:38 -0500

    solved 1748A

---
+++ Codeforces/1748A.cpp
@@ -0,0 +1,25 @@
+#pragma GCC target("avx2")
+#pragma GCC optimize("03")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef long double ld;
+typedef pair<ll, ll> pl;
+
+// macros
+#define G(x) ll x; cin >> x;                                    // read ll from cin and store its value
+#define F(i, start, end) for (ll i = start; i < (end); i++)     // shortened for loop
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    // SOLUTION HERE
+    G(t)
+    while (t--) {
+        G(n);
+        cout << (n + 1) / 2 << '\n';
+    }
+}
